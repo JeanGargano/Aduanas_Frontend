0e765786620508455a2fe865fbc8a4309be6d275
JeanEsg <160681687+JeanEsg@users.noreply.github.com>
2025-09-08 12:19:22 -0500

    delete a no use imporation component

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,8 +1,5 @@
 import "./RightSide.css";
 import Updates from "../../pages/Updates/Updates";
-import Clientes from "../../pages/Clientes/Clientes";
-import CustomerReview from "../CustomerReview/CustomerReview";
-
 
 const RightSide = () => {
     return (
@@ -10,10 +7,6 @@ const RightSide = () => {
             <div>
                 <h3>Últimas actualizaciones</h3>
                 <Updates />
-            </div>
-            <div>
-                <h3>Clientes</h3>
-                <CustomerReview />
             </div>
         </div>
     )
