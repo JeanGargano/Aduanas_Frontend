d46ddeb5f79a8c8cca86a970e691bdd2e5b1afed
JeanEsg <160681687+JeanEsg@users.noreply.github.com>
2025-09-08 12:26:33 -0500

    fix hook to calculate last month

--- src/hooks/useUsuarios.js
+++ src/hooks/useUsuarios.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import { listarUsuarios } from "../Services/usuariosApi"; // Ajusta la ruta si es necesario
+import { listarUsuarios } from "../Services/usuariosApi";
 
 const CACHE_KEY = "usuarios";
 const CACHE_TIME_KEY = "usuarios_cache_time";
-const CACHE_DURATION = 10 * 60 * 1000; // 10 minutos en milisegundos
+const CACHE_RESUMEN_KEY = "usuarios_resumen";
+const CACHE_DURATION = 10 * 60 * 1000; // 10 minutos
 
 export const useUsuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
+  const [resumen, setResumen] = useState({
+    cantidadTotalUsuarios: 0,
+  });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -14,19 +18,34 @@ export const useUsuarios = () => {
     const cargarUsuarios = async () => {
       const cache = localStorage.getItem(CACHE_KEY);
       const cacheTime = localStorage.getItem(CACHE_TIME_KEY);
+      const cacheResumen = localStorage.getItem(CACHE_RESUMEN_KEY);
       const now = Date.now();
 
+      // Si hay cache válido
       if (cache && cacheTime && now - Number(cacheTime) < CACHE_DURATION) {
         setUsuarios(JSON.parse(cache));
+        if (cacheResumen) {
+          setResumen(JSON.parse(cacheResumen));
+        }
         setLoading(false);
         return;
       }
 
       try {
+        let datosArray = [];
         const data = await listarUsuarios();
+        datosArray = Array.isArray(data) ? data : [];
+        const cantidadTotalUsuarios = datosArray.length;
+
+        // resumen
+        const nuevoResumen = { cantidadTotalUsuarios };
+        setResumen(nuevoResumen);
+        localStorage.setItem(CACHE_RESUMEN_KEY, JSON.stringify(nuevoResumen));
+
+        // usuarios con id
         const usuariosConId = data.map((u) => ({
           ...u,
-          id: u.identificacion, // Establece 'id' para uso en tablas
+          id: u.identificacion, // clave única para tablas
         }));
 
         setUsuarios(usuariosConId);
@@ -43,5 +62,5 @@ export const useUsuarios = () => {
     cargarUsuarios();
   }, []);
 
-  return { usuarios, loading, error };
+  return { usuarios, resumen, loading, error };
 };
