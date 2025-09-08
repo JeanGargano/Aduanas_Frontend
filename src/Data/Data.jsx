8244956a523e6ca36ad43eddcd9b366e1f0995ed
JeanEsg <160681687+JeanEsg@users.noreply.github.com>
2025-09-08 12:20:54 -0500

    uodate to use dinamic data

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -3,19 +3,16 @@ import {
   UilEstate,
   UilClipboardAlt,
   UilUsersAlt,
-  UilPackage,
   UilChart,
   UilUsdSquare,
   UilMoneyWithdrawal,
 } from "@iconscout/react-unicons";
-import { minWidth, width } from "@mui/system";
-import { Link } from "react-router-dom";
 
 //Sidebar Data
 export const SidebarData = [
   {
     icon: UilEstate,
-    heading: "Dashboard",
+    heading: "Inicio",
     link: "/dashboard",
     roles: ["Administrador", "Cliente"],
   },
@@ -33,26 +30,33 @@ export const SidebarData = [
   },
   {
     icon: UilChart,
-    heading: "Analytics",
+    heading: "Analíticas",
     link: "/analytics",
-    roles: ["Administrador"],
+    roles: ["Administrador", "Cliente"],
   },
 ];
 
-export const cardsData = [
+// función para calcular porcentaje sin NaN
+const calcularPorcentaje = (parte, total) => {
+  if (!total || total === 0) return 0;
+  return ((parte / total) * 100).toFixed(0);
+};
+
+
+export const generarCardsPedidos = (resumen) => [
   {
     title: "Pedidos",
     color: {
       backGround: "linear-gradient(180deg, #E7423E 0%, 	#E7423E 100%)",
       boxShadow: "0px 10px 20px 0px #E7423E",
     },
-    barValue: 99,
-    value: "120",
+    barValue: 100,
+    value: `${resumen.cantidadTotalPedidos}`,
     png: UilUsdSquare,
     series: [
       {
-        name: "Sales",
-        data: [31, 40, 28, 51, 42, 109, 100],
+        name: "Pedidos",
+        data: resumen.seriesPedidos,
       },
     ],
   },
@@ -62,50 +66,35 @@ export const cardsData = [
       backGround: "linear-gradient(180deg,#6E81A4 0%, #6E81A4 100%)",
       boxShadow: "0px 10px 20px 0px #6E81A4",
     },
-    barValue: 40,
-    value: "40",
+    barValue: calcularPorcentaje(resumen.cantidadEntregados, resumen.cantidadTotalPedidos),
+    value: `${resumen.cantidadEntregados}`,
     png: UilMoneyWithdrawal,
     series: [
       {
-        name: "Revenue",
-        data: [10, 100, 50, 70, 80, 30, 40],
+        name: "Entregados",
+        data: resumen.seriesEntregados,
       },
     ],
   },
   {
-    title: "Procesos Activos",
+    title: "En Proceso",
     color: {
       backGround: "linear-gradient(180deg, #052462 0%, #052462 100%)",
       boxShadow: "0px 10px 20px 0px #052462",
     },
-    barValue: 60,
-    value: "80",
+    barValue: calcularPorcentaje(resumen.cantidadEnProceso, resumen.cantidadTotalPedidos),
+    value: `${resumen.cantidadEnProceso}`,
     png: UilClipboardAlt,
     series: [
       {
-        name: "Expenses",
-        data: [10, 25, 15, 30, 12, 15, 20],
+        name: "En Proceso",
+        data: resumen.seriesEnProceso,
       },
     ],
   },
 ];
 
-export const UpdatesData = [
-  {
-    name: "Andrew Thomas",
-    noti: "El estado de su pedido ha cambiado a Entregado.",
-    time: "25 seconds ago",
-  },
-  {
-    name: "James Bond",
-    noti: "El estado de su pedido ha cambiado a Enviado.",
-    time: "30 minutes ago",
-  },
-  {
-    name: "Iron Man",
-    noti: "El estado de su pedido ha cambiado a Recibido.",
-    time: "2 hours ago",
-  },
-];
+
+
 
 
